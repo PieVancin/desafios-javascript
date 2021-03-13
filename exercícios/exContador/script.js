6e3ef6c1f66ce107beea70a8450192881ecbbd21
Pierre <pie.vancin@gmail.com>
2021-03-13 20:11:52 -0300

    wip: exercício contador

---
+++ exercícios/exContador/script.js
@@ -0,0 +1,12 @@
+function contar() {
+    let inicio = document.getElementById("inicio")
+    let fim = document.getElementById("fim")
+    let passso = document.getElementById("passo")
+    let resultado = document.getElementById("resultado")
+    for (inicio; inicio < fim; passo) {
+        if (inicio <= 0) {
+            resultado.innerHTML = eee
+        }
+        document.getElementById("resultado").innerHTML = `${passo}`
+    }
+}
