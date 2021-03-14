8264a227ee234f4706ab3108ad7fb0e9a317b109
Pierre <pie.vancin@gmail.com>
2021-03-13 23:22:49 -0300

    style: comentários adicionados

--- exercícios/exContador/script.js
+++ exercícios/exContador/script.js
@@ -4,6 +4,7 @@ function contar() {
     let passso = document.getElementById("passo")
     let resultado = document.getElementById("resultado")
 
+    // verifica se algum dos valores é igual a zero
     if (inicio.value.length == 0 || passso.value.length == 0 || fim.value.length == 0) {
         window.alert('Erro')
     } else {
@@ -13,16 +14,20 @@ function contar() {
     let f = Number(fim.value)
     let p = Number(passo.value)
 
+    // condição para o navegador não crashar
     if (p <= 0) {
         resultado.innerHTML = `Passo inválido, considerando passo 1:<br>`
         p = 1
     }
 
+    // faz a contagem em ordem crescente
+    // a expressão += no innerHTML serve para concatenar o que for escrito com o que já está no documento
     if (ini < f) {
         for (ini; ini <= f; ini += p) {
             resultado.innerHTML += `${ini} \u{1F449}`
         }
         resultado.innerHTML += `\u{1F3C1}`
+    // faz a contagem em ordem decrescente        
     } else {
         for (ini; ini >= f; ini -= p) {
             resultado.innerHTML += `${ini} \u{1F449}`
