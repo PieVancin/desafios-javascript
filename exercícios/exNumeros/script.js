8ee3799e4611bf295afeb937bacfd999b42f3af2
Pierre <pie.vancin@gmail.com>
2021-03-21 19:26:17 -0300

    wip: analisador de números

--- exercícios/exNumeros/script.js
+++ exercícios/exNumeros/script.js
@@ -1,10 +1,17 @@
-function adicionar() {
-    let res = document.querySelector("#num")
-    let lista = document.getElementById("lista")
-    let opt = document.querySelector("#lista")
-    let numero = document.getElementById("num")
-    let numeros = []
+let res = document.querySelector("#resultado")
+let lista = document.getElementById("lista")
+let opt = document.querySelector("#lista")
+let numero = document.getElementById("num")
+let numeros = []
 
+function adicionar() {
     let num = Number(numero.value)
     opt.innerHTML += `<option value="${num}">${num}</option>`
+    numeros.push(`${num}`)
+}
+
+function finalizar() {
+    for (let i = 0; i< numeros.length; i++) {
+        res.innerHTML += `${numeros[i]}<p>`
+    }
 }
